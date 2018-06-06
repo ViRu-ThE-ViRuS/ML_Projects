63e422abc2b078d2c75566c1cc7f3759f8b08d28
Viraat_Chandra <viraat.chandra@hotmail.com>
2018-06-06 15:56:25 +0530

    Optimized RNN and updated plotting parameters

--- ModelFocused/NeuralNetwork/Recurrent/RNN [tensorflow].py
+++ ModelFocused/NeuralNetwork/Recurrent/RNN [tensorflow].py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 num_epochs = 100
 total_series_length = 50000
 truncated_backprop_length = 15
-state_size = 4
+state_size = 4 # ie number of neurons in hidden layer
 num_classes = 2
 echo_step = 3
 batch_size = 5
@@ -21,7 +21,7 @@ def generateData():
     x = x.reshape((batch_size, -1))
     y = y.reshape((batch_size, -1))
 
-    return (x, y)
+    return [x, y]
 
 batchX_placeholder = tf.placeholder(tf.float32, [batch_size, truncated_backprop_length])
 batchY_placeholder = tf.placeholder(tf.int32, [batch_size, truncated_backprop_length])
@@ -30,11 +30,11 @@ init_state = tf.placeholder(tf.float32, [batch_size, state_size])
 
 # hidden layer
 W = tf.Variable(np.random.rand(state_size+1, state_size), dtype=tf.float32)
-b = tf.Variable(np.zeros((1,state_size)), dtype=tf.float32)
+b = tf.Variable(np.zeros([1,state_size]), dtype=tf.float32)
 
 # output layer
 W2 = tf.Variable(np.random.rand(state_size, num_classes),dtype=tf.float32)
-b2 = tf.Variable(np.zeros((1,num_classes)), dtype=tf.float32)
+b2 = tf.Variable(np.zeros([1,num_classes]), dtype=tf.float32)
 
 # unstack columns
 # flatten the tensor with input vectors for each batch (len = truncated_backprop_length)
@@ -59,12 +59,12 @@ predictions_series = [tf.nn.softmax(logits) for logits in logits_series]
 losses = [tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels) for logits, labels in zip(logits_series,labels_series)]
 total_loss = tf.reduce_mean(losses)
 
-train_step = tf.train.AdagradOptimizer(0.3).minimize(total_loss) # RMSPropOptimizer might actually work better and get faster convergence
+train_step = tf.train.RMSPropOptimizer(0.03).minimize(total_loss) # faster convergance than AdagradOptimizer(0.3)
 
 def plot(loss_list, predictions_series, batchX, batchY):
     plt.subplot(2, 3, 1)
     plt.cla()
-    plt.plot(loss_list)
+    plt.plot(loss_list, color='g')
 
     for batch_series_idx in range(5):
         one_hot_output_series = np.array(predictions_series)[:, batch_series_idx, :]
@@ -74,16 +74,16 @@ def plot(loss_list, predictions_series, batchX, batchY):
         plt.cla()
         plt.axis([0, truncated_backprop_length, 0, 2])
         left_offset = range(truncated_backprop_length)
-        plt.bar(left_offset, batchX[batch_series_idx, :], width=1, color="blue")
-        plt.bar(left_offset, batchY[batch_series_idx, :] * 0.5, width=1, color="red")
-        plt.bar(left_offset, single_output_series * 0.3, width=1, color="green")
+        plt.bar(left_offset, batchX[batch_series_idx, :], width=1, color="c")
+        plt.bar(left_offset, batchY[batch_series_idx, :] * 0.5, width=1, color="b")
+        plt.bar(left_offset, single_output_series * 0.3, width=1, color="g")
 
     plt.draw()
     plt.pause(0.0001)
 
 
 with tf.Session() as sess:
-    sess.run(tf.initialize_all_variables())
+    sess.run(tf.global_variables_initializer())
     plt.ion()
     plt.figure()
     plt.show()
@@ -91,7 +91,7 @@ with tf.Session() as sess:
 
     for epoch_idx in range(num_epochs):
         x,y = generateData()
-        _current_state = np.zeros((batch_size, state_size))
+        _current_state = np.zeros([batch_size, state_size])
 
         print("New data, epoch", epoch_idx)
 
@@ -99,8 +99,8 @@ with tf.Session() as sess:
             start_idx = batch_idx * truncated_backprop_length
             end_idx = start_idx + truncated_backprop_length
 
-            batchX = x[:,start_idx:end_idx]
-            batchY = y[:,start_idx:end_idx]
+            batchX = x[:,start_idx:end_idx] # [batch_size, truncated_backprop_length]
+            batchY = y[:,start_idx:end_idx] # [batch_size, truncated_backprop_length]
 
             _total_loss, _train_step, _current_state, _predictions_series = sess.run(
                 [total_loss, train_step, current_state, predictions_series],
@@ -120,3 +120,4 @@ plt.ioff()
 plt.show()
 
 # no need for testing data, the model will be optimial in approximating the desired function (random in this case)
+# try out this program with another weight matrix U for the hidden state and concatenate after broadcast addition
